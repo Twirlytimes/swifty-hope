50b0ded35159a43e4b7291fb01981dad5679f7fa
Twirlytimes <future.sparkle@gmail.com>
2014-11-03 21:28:23 +0100

    Initial commit from local git

---
+++ hello-swift/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  hello-swift
+//
+//  Created by SVI on 03/11/2014.
+//  Copyright (c) 2014 SVI. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
+var name = "swift"
+
+println("Hello " + name)
+
